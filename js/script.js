9bfc5d396889c8d5d98978d96ce2b98ac28fbc00
Andrew-Minukovich <85671356+Andrew-Minukovich@users.noreply.github.com>
2021-09-26 23:30:49 +0300

    feature: add select dropdown

--- js/script.js
+++ js/script.js
@@ -11,3 +11,35 @@ const modalWindow = () => {
 }
 
 modalWindow();
+
+
+const selectDropDown = () => {
+    const arrow = document.querySelector('.sort__select-arrow');
+    const dropdown = document.querySelector('.sort__dropdown');
+    const content = document.querySelector('.sort__dropdown-content')
+    const selectTitle = document.querySelector('.sort__select-title');
+
+    const manageDropdown = () => {
+        let flag = false;
+        return () => {
+            flag = !flag;
+            if (flag) {
+                arrow.classList.add('_open');
+                dropdown.style.display = 'block';
+            } else {
+                arrow.classList.remove('_open');
+                dropdown.style.display = 'none';
+            }
+        }
+    }
+
+    const selectItem = (e) => {
+        selectTitle.innerHTML = e.target.innerHTML;
+    }
+
+    arrow.addEventListener('click', manageDropdown());
+    content.addEventListener('click', selectItem)
+
+}
+
+selectDropDown();
