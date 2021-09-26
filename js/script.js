7d755cf5f1932b067c82087f817968a51d351eb0
Andrew-Minukovich <85671356+Andrew-Minukovich@users.noreply.github.com>
2021-09-26 13:58:51 +0300

    feature: add filters modal

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const modalWindow = () => {
+    const modal = document.querySelector('.filters-modal');
+    const closeButton = document.querySelector('.filters-modal-close');
+    const openButton = document.querySelector('.catalog__filters-button');
+    openButton.addEventListener('click', () => {
+        modal.style.display = 'block';
+    })
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    })
+}
+
+modalWindow();
