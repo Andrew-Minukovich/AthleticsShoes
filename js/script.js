404a828c922db31437dc516d75b81305e1216e03
Andrei Miniukovich <Andrei.Miniukovich@itechart-group.com>
2021-09-27 16:56:36 +0300

    fix: fix web accessebility properties

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ const modalWindow = () => {
     const closeButton = document.querySelector('.filters-modal-close');
     const openButton = document.querySelector('.catalog__filters-button');
     openButton.addEventListener('click', () => {
-        modal.style.display = 'block';
+        modal.classList.toggle('filters-modal--open', true);
+        closeButton.focus();
     })
     closeButton.addEventListener('click', () => {
-        modal.style.display = 'none';
+        modal.classList.toggle('filters-modal--open', false);
     })
 }
 
